c072102227c26e4571a287a0226b5bcdda897574
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-06-04 16:59:39 +0700

    refactor(bricks): change logger progress message

--- bricks/repo/hooks/pre_gen.dart
+++ bricks/repo/hooks/pre_gen.dart
@@ -1,7 +1,7 @@
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final progress = context.logger.progress('Generating use case...');
+  final progress = context.logger.progress('Generating repository...');
   final repoType = context.vars['repo_type'];
   final returnType = context.vars['return_type'];
 
